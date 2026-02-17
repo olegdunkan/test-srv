546034917c2d7a08ff4c77b5ee6b0d2a1b390306
olegdunkan <olegjobs@mail.ru>
2026-02-17 11:23:26 +0000

    change to v1

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 app.get(
   '/',
   (req: Request<any>, res: Response<EchoResponse>) => {
-    return res.json({ message: 'Hello, World!' });
+    return res.json({ message: 'Hello, World v.1!' });
   }
 );
 app.get(
